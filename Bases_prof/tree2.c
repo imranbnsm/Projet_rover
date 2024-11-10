30ee5750530cd0c0f5c0432e3f68ba8105e7ffa4
Enzo-JZN <130388912+Enzo-JZN@users.noreply.github.com>
2024-11-10 22:05:03 +0100

    Update tree2.c

--- Bases_prof/tree2.c
+++ Bases_prof/tree2.c
@@ -65,3 +65,37 @@ void displayTree(t_node *root, int depth)
         displayTree(root->children[i], depth + 1);
     }
 }
+
+// recherche d'une feuille de valeur minimale 
+
+/* On crée une fonction auxilaire pour pouvoir faire une fonction recursive, qui prendre en argument le noeud où on se situe, la position de la station,
+le noeud avec le coup minimum actuel, et sa valeur. */
+t_node *SearchLeafMinAuxiliaire(t_node *node, t_localisation loc, t_node **min_cost_node, int *min_cost) {
+    if (node == NULL) return NULL;
+
+    // Vérifie si le noeud actuel se situe à la sation de base et s'il a un coût inférieur au minimum actuel.
+    if (node->loc.x == loc.x && node->loc.y == loc.y && node->cost < *min_cost) {
+        *min_cost = node->cost;
+        *min_cost_node = node;
+    }
+
+    // Y a plusieurs noeuds qui ont pour position la station de base, c'est pour ça qu'on continue de parcourir d'autres noeuds pour savoir lequel est le plus efficace.
+    for (int i = 0; i < node->num_children; i++) {
+        SearchLeafMinAuxiliaire(node->children[i], loc, min_cost_node, min_cost); // On appelle la fonction recursivement pour trouver le noeud avec le coût le plus bas.
+    }
+
+    return *min_cost_node;
+}
+
+/* On va creer une fonction qui va nous donner la feuille avec la valeur la plus petite, pour faire ça on va prendre en argument l'arbre en question, 
+et la position finale où l'on veut être c'est à dire la station de base */
+t_node *SearchLeafMin(t_tree tree, t_localisation loc) {
+    t_node *min_cost_node = NULL; // Noeud avec le cout minimum.
+    int min_cost = 10000;         
+
+    // Appelle de la fonction auxiliaire pour rechercher le noeud feuille avec le coût minimum
+    return SearchLeafMinAuxiliaire(tree.root, loc, &min_cost_node, &min_cost);
+}
+
+
+
